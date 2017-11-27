ad428c83ad84bb949b976b3667cb9feb922aa6cc
KrunalGote <33930847+KrunalGote@users.noreply.github.com>
2017-11-27 11:30:43 +0530

    Adding Proper Comments

--- TestYodoConvertor/YodoConvertorService/ConvertorService.cs
+++ TestYodoConvertor/YodoConvertorService/ConvertorService.cs
@@ -12,16 +12,23 @@ namespace YodoConvertorService
     {
         public async Task<string> ConvertEnglishToYodoSpeak(string input)
         {
-            //throw new NotImplementedException();
-            //return input;
+            //Create HttpCilent to consume service            
             HttpClient client = new HttpClient();
             
+            //Add Base Address
             client.BaseAddress = new Uri("https://yoda.p.mashape.com/yoda");
+            
+            // Clear Header
             client.DefaultRequestHeaders.Accept.Clear();
+            
+            //Add Header
             client.DefaultRequestHeaders.Add("X-Mashape-Key", "b2IuNDRHZPmshrOCPURlLwc62ayep1LkgkPjsngPT02HcnEkjV");
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("text/plain"));
             
+            //Call Yodo Speak Rest API client
             var result=await client.GetAsync(string.Format("?sentence={0}",input));
+            
+            //Check Status of Respose.
             if(result.IsSuccessStatusCode)
             {
                 return await result.Content.ReadAsStringAsync();
