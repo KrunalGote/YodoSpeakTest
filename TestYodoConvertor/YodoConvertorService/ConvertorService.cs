e4b64dfc8607159c632e40c858efb28feb32f65a
KrunalGote <33930847+KrunalGote@users.noreply.github.com>
2017-11-30 00:52:57 +0530

    Added Constant

--- TestYodoConvertor/YodoConvertorService/ConvertorService.cs
+++ TestYodoConvertor/YodoConvertorService/ConvertorService.cs
@@ -12,30 +12,21 @@ namespace YodoConvertorService
     {
         public async Task<string> ConvertEnglishToYodoSpeak(string input)
         {
-            //Create HttpCilent to consume service            
             HttpClient client = new HttpClient();
             
-            //Add Base Address
-            client.BaseAddress = new Uri("https://yoda.p.mashape.com/yoda");
-            
-            // Clear Header
+            client.BaseAddress = new Uri(Utilies.YodoSpeakApiUrl);
             client.DefaultRequestHeaders.Accept.Clear();
-            
-            //Add Header
-            client.DefaultRequestHeaders.Add("X-Mashape-Key", "b2IuNDRHZPmshrOCPURlLwc62ayep1LkgkPjsngPT02HcnEkjV");
+            client.DefaultRequestHeaders.Add(Utilies.HeaderMashapeKey, Utilies.HeaderMashapeValue);
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("text/plain"));
             
-            //Call Yodo Speak Rest API client
-            var result=await client.GetAsync(string.Format("?sentence={0}",input));
-            
-            //Check Status of Respose.
+            var result=await client.GetAsync(string.Format("{0}",input));
             if(result.IsSuccessStatusCode)
             {
                 return await result.Content.ReadAsStringAsync();
             }
             else
             {
-                return "Sorry. Yodo service is down.";
+                return Utilies.ApiDownMessage; ;
             }
         }
     }
---
+++ TestYodoConvertor/YodoConvertorService/Utilies.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YodoConvertorService
+{
+   public static class Utilies
+    {
+        public const string YodoSpeakApiUrl= "https://yoda.p.mashape.com/yoda?sentence=";
+        public const string HeaderMashapeKey = "X-Mashape-Key";
+        public const string HeaderMashapeValue = "b2IuNDRHZPmshrOCPURlLwc62ayep1LkgkPjsngPT02HcnEkjV";
+        public const string ApiDownMessage = "Sorry. Yodo service is down.";
+    }
+}
